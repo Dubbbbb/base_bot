d85aaa58581179c73f16a32c3fd977155cb849e6
ArtsemDev <thommy.python.dev@gmail.com>
2023-12-08 17:30:41 +0300

    add custom list[int] type

--- app/src/types/custom.py
+++ app/src/types/custom.py
@@ -4,7 +4,15 @@ from pydantic import AfterValidator
 
 __all__ = [
     "ListStrEnv",
+    "ListIntEnv",
 ]
 
 
-ListStrEnv = Annotated[str | list[str], AfterValidator(func=lambda x: x.split(",") if isinstance(x, str) else x)]
+ListStrEnv = Annotated[
+    str | list[str],
+    AfterValidator(func=lambda x: x.split(",") if isinstance(x, str) else x)
+]
+ListIntEnv = Annotated[
+    str | list[int],
+    AfterValidator(func=lambda x: [int(val) for val in x.split(",")] if isinstance(x, str) else x)
+]
