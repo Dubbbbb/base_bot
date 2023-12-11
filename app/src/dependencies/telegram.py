dc5cc89984e51cc37b37602dc4dce6cd3a950176
Artem Protaev <a.protaev@enigma.global>
2023-12-11 12:41:28 +0300

    add docstring

--- app/src/dependencies/telegram.py
+++ app/src/dependencies/telegram.py
@@ -11,6 +11,11 @@ __all__ = [
 
 
 async def telegram_secret_token_exist(x_telegram_bot_api_secret_token: str = Header()) -> NoReturn:
+    """validate X-Telegram-Bot-Api-Secret-Token header
+
+    :param x_telegram_bot_api_secret_token: header value
+    :raise HTTPException: 403 if invalid header value
+    """
     if x_telegram_bot_api_secret_token != settings.TELEGRAM_SECRET_TOKEN.get_secret_value():
         raise HTTPException(status_code=HTTP_403_FORBIDDEN)
 
