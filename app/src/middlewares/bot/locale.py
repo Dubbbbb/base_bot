be794b24929307a018380543816b30726e2ac6bb
Artem Protaev <a.protaev@enigma.global>
2023-12-11 12:46:57 +0300

    add docstring

--- app/src/middlewares/bot/locale.py
+++ app/src/middlewares/bot/locale.py
@@ -13,6 +13,7 @@ __all__ = [
 
 
 class L10N(object):
+    """fluent.runtime manager"""
 
     def __init__(
             self,
@@ -21,6 +22,13 @@ class L10N(object):
             resource_ids: list[str],
             roots: str | list[str],
     ) -> None:
+        """init
+
+        :param locales: allowed locales
+        :param default_locale: default locale
+        :param resource_ids: file names
+        :param roots: path to locale folder
+        """
         self.default_locale = default_locale
         self.loader = FluentResourceLoader(roots=roots)
         self.localizations = {
@@ -33,17 +41,32 @@ class L10N(object):
         }
 
     def get(self, locale: str) -> FluentLocalization:
+        """getting fluent by user locale
+
+        :param locale: user locale
+        :return: FluentLocalization object
+        """
         if locale in self.localizations:
             return self.localizations.get(locale)
         return self.localizations.get(self.default_locale)
 
     def __getitem__(self, item: str) -> FluentLocalization:
+        """getting fluent by user locale
+
+        :param item: user locale
+        :return: FluentLocalization object
+        """
         return self.get(locale=item)
 
 
 class L10NMiddleware(BaseMiddleware):
+    """localization middleware"""
 
     def __init__(self, l10n: L10N) -> None:
+        """init
+
+        :param l10n: L10N object
+        """
         self.l10n = l10n
 
     async def __call__(
@@ -52,6 +75,7 @@ class L10NMiddleware(BaseMiddleware):
             event: Message | CallbackQuery,
             data: Dict[str, Any]
     ) -> Any:
+        """implementation of the FluentLocalization according to the language of the user in handlers"""
         user_locale = event.from_user.language_code or settings.DEFAULT_LOCALE
         data["_"] = self.l10n.get(locale=user_locale).format_value
         return await handler(event, data)
